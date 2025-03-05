049d74ca3b5305e62fdc88932850dc4ad156a6a5
pepab0t <cernikjos@gmail.com>
2025-03-05 21:06:52 +0100

    test: piece registration auto name

--- tests/test_auto_name.py
+++ tests/test_auto_name.py
@@ -1,6 +1,8 @@
+from pieceful import Piece, PieceFactory
+from pieceful.registry import registry
+
 from .models import AbstractEngine
-from .setup import refresh_after
-from pieceful import Piece
+from .setup import refresh_after  # noqa: F401
 
 
 def test_register_class_with_decorator_without_name_spec():
@@ -8,5 +10,21 @@ def test_register_class_with_decorator_without_name_spec():
     class Engine(AbstractEngine):
         pass
 
-    assert Engine in refresh_after()
-    assert refresh_after()[Engine].name == "Engine"
+    assert registry.get_object("Engine", AbstractEngine) is not None
+    assert registry.get_object("Engine", Engine) is not None
+    assert "Engine" in registry.registry
+    assert len(registry.registry["Engine"]) == 1
+
+
+def test_register_factory_with_decorator_without_name_spec():
+    class Engine(AbstractEngine):
+        pass
+
+    @PieceFactory()
+    def engine_factory() -> Engine:
+        return Engine()
+
+    assert registry.get_object("engine_factory", AbstractEngine) is not None
+    assert registry.get_object("engine_factory", Engine) is not None
+    assert "engine_factory" in registry.registry
+    assert len(registry.registry["engine_factory"]) == 1
