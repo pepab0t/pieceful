adf2cb90535803273ee61d20f9b85f94aa927341
pepab0t <cernikjos@gmail.com>
2025-03-19 21:05:46 +0100

    feat: progress is_subclass

--- pieceful/typing_utils.py
+++ pieceful/typing_utils.py
@@ -1,11 +1,13 @@
 from functools import lru_cache
-from typing import Generic, Type, get_origin
+from re import A
+from types import UnionType
+from typing import Annotated, Any, Generic, Literal, Type, TypeAliasType, Union, get_args, get_origin
 
 
-# @lru_cache
+@lru_cache(typed=True)
 def is_generic(cls: Type[object]) -> bool:
     # If cls is a typing generic alias (e.g., List, Dict), get_origin(cls) is not None
-    if get_origin(cls) is not None:
+    if (get_origin(cls)) not in (None, Annotated, Literal, Union, UnionType):
         return True
 
     # If cls is a user-defined generic, it should have Generic in its bases
@@ -17,3 +19,43 @@ def is_generic(cls: Type[object]) -> bool:
         return True
 
     return False
+
+
+def _resolve_type_alias(cls):
+    while cls.__class__ is TypeAliasType:
+        cls = cls.__value__
+    return cls
+
+
+def is_subclass(cls, parent) -> bool:
+    # TODO union? literal?
+    cls = _resolve_type_alias(cls)
+    parent = _resolve_type_alias(parent)
+
+    if parent is object or parent is Any:
+        return True
+
+    if cls.__class__ is not parent.__class__:
+        if get_origin(parent) in (UnionType, Union):
+            return any(is_subclass(cls, arg) for arg in get_args(parent))
+        if get_origin(cls) is Literal:
+            return all(is_subclass(getattr(arg, "__orig_class__", arg.__class__), parent) for arg in get_args(cls))
+        if get_origin(parent) is Annotated:
+            return is_subclass(cls, get_args(parent)[0])
+        if get_origin(cls) is Annotated:
+            return is_subclass(get_args(cls)[0], parent)
+        if cls.__class__ is type and is_generic(parent):
+            return any(is_subclass(base, parent) for base in cls.__orig_bases__)
+        return False
+
+    if is_generic(cls):
+        return issubclass(get_origin(cls), get_origin(parent)) and get_args(cls) == get_args(parent)
+
+    if get_origin(cls) is Literal:
+        parent_args = set(get_args(parent))
+        return len(set(get_args(cls)) - parent_args) == 0
+
+    if get_origin(parent) is Annotated:
+        return is_subclass(get_args(cls)[0], get_args(parent)[0])
+
+    return issubclass(cls, parent)
--- tests/test_typing_utils.py
+++ tests/test_typing_utils.py
@@ -1,6 +1,6 @@
-from typing import Dict, Generic, List, Set, Tuple, TypeVar
+from typing import Annotated, Any, Dict, Generic, List, Literal, Set, Tuple, TypeVar, Union
 
-from pieceful.typing_utils import is_generic
+from pieceful.typing_utils import is_generic, is_subclass
 
 
 def test_is_generic():
@@ -43,3 +43,83 @@ def test_is_generic():
     assert not is_generic(str)
     assert not is_generic(float)
     assert not is_generic(bool)
+    assert not is_generic(Any)
+    assert not is_generic(Literal[1])
+    assert not is_generic(Annotated[int, "a"])
+    assert not is_generic(int | str)
+    assert not is_generic(Union[int, str])
+
+
+def test_is_subclass():
+    class Armor:
+        pass
+
+    class ChestArmor(Armor):
+        pass
+
+    class Inventory[T]:
+        pass
+
+    class SmallInventory[T](Inventory[T]):
+        def __init__(self, value: T | None = None) -> None:
+            self.value = value
+
+        def __eq__(self, other: object) -> bool:
+            return isinstance(other, SmallInventory) and self.value == other.value
+
+        def __hash__(self) -> int:
+            return hash(self.value)
+
+    class ArmorInventory(Inventory[Armor]):
+        pass
+
+    class Character:
+        pass
+
+    class Mage(Character):
+        pass
+
+    assert is_subclass(Character, Character)
+    assert is_subclass(Mage, Character)
+    assert is_subclass(Inventory[int], Inventory[int])
+    assert is_subclass(SmallInventory[int], Inventory[int])
+    assert is_subclass(ArmorInventory, Inventory[Armor])
+    assert is_subclass(Character, object)
+    assert is_subclass(Character, Any)
+    assert is_subclass(Inventory[int], object)
+    assert is_subclass(Inventory[int], Any)
+    assert is_subclass(Literal[1], Any)
+    assert is_subclass(Literal[1], object)
+    assert is_subclass(Literal[1], Literal[1])
+    assert is_subclass(Literal[1], Literal[1, 2, 3])
+    assert is_subclass(Literal[1], Literal[1, "string"])
+    assert is_subclass(Literal[1], int)
+    assert is_subclass(Literal[1, 2, 3], int)
+    assert is_subclass(int, int | str)
+    assert is_subclass(Literal[1], Literal[1] | Literal[2])
+    str_inventory = Inventory[str]()
+    assert is_subclass(Literal[str_inventory], Literal[str_inventory, Inventory[int]()])
+    assert is_subclass(Literal["a", "b"], Literal["a", "b", "c"])
+    assert is_subclass(Literal[SmallInventory[int]()], Literal[SmallInventory[int]()])  # has hash and equals
+    assert is_subclass(Inventory[int], Inventory[str] | Inventory[int])
+    assert is_subclass(Inventory[Literal[1]], Inventory[Literal[1]] | Inventory[str])
+    assert is_subclass(Annotated[int, "a"], int)
+    assert is_subclass(int, Annotated[int, "a"])
+    assert is_subclass(Annotated[int, "a"], Annotated[int, "a"])
+    assert is_subclass(Annotated[int, "b"], Annotated[int, "a"])
+    assert is_subclass(Annotated[Inventory[int], "b"], Annotated[Inventory[int], "a"])
+    assert is_subclass(Annotated[int, "b"], Annotated[int | str, "a"])
+    assert is_subclass(Annotated[Literal[2], "b"], Annotated[Literal[1, 33] | Literal[2], "a"])
+
+    assert not is_subclass(Inventory[int], Inventory)
+    assert not is_subclass(Inventory, Inventory[int])
+    assert not is_subclass(Inventory[ChestArmor], Inventory[Armor])
+    assert not is_subclass(Inventory[Armor], Inventory[Any])
+    assert not is_subclass(SmallInventory[ChestArmor], Inventory[Armor])
+    assert not is_subclass(int, Literal[1])
+    assert not is_subclass(Literal[1, "string"], int)
+    assert not is_subclass(int, str)
+    assert not is_subclass(Literal[ArmorInventory()], Literal[Inventory[Armor]()])
+    assert not is_subclass(Literal[Inventory[str]()], Literal[Inventory[int]()])
+    assert not is_subclass(Literal[Inventory[str]()], Literal[Inventory[str]()])
+    assert not is_subclass(Annotated[Inventory[ChestArmor], "b"], Annotated[Inventory[Armor], "a"])
